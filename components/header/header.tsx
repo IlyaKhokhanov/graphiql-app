0fec5697f0a6ae51e7e8ff95998a6ea8004b8b4d
anturchin <tur4intwo@gmail.com>
2024-08-31 20:05:56 +0300

    feat: update header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,29 +4,48 @@ import Link from 'next/link';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { IntlProvider } from 'react-intl';
 import { FormattedMessage } from 'react-intl';
+import { usePathname, useRouter } from 'next/navigation';
 
 import { auth, logout } from '@/services/firebase';
 import { Button, LocaleSelector } from '@/components';
 import { IHeaderProps } from './header.props';
 
+import styles from './header.module.css';
+
 export const Header = ({ locale, messages }: IHeaderProps) => {
   const [user] = useAuthState(auth);
 
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const onHandleClick = (path: string): void => {
+    router.push(path);
+  };
+
   return (
     <IntlProvider locale={locale} messages={messages}>
-      <header style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 10 }}>
+      <header className={styles.header}>
         {user ? (
           <>
-            <Link href={`/${locale}`}>
+            <Link className={pathname === `/${locale}` ? styles.active : ''} href={`/${locale}`}>
               <FormattedMessage id="links.home" />
             </Link>
-            <Link href={`/${locale}/rest/GET/json`}>
+            <Link
+              className={pathname === `/${locale}/rest/GET/json` ? styles.active : ''}
+              href={`/${locale}/rest/GET/json`}
+            >
               <FormattedMessage id="links.rest" />
             </Link>
-            <Link href={`/${locale}/graphql/query`}>
+            <Link
+              className={pathname === `/${locale}/graphql/query` ? styles.active : ''}
+              href={`/${locale}/graphql/query`}
+            >
               <FormattedMessage id="links.graphql" />
             </Link>
-            <Link href={`/${locale}/history`}>
+            <Link
+              className={pathname === `/${locale}/history` ? styles.active : ''}
+              href={`/${locale}/history`}
+            >
               <FormattedMessage id="links.history" />
             </Link>
             <Button
@@ -39,15 +58,14 @@ export const Header = ({ locale, messages }: IHeaderProps) => {
           </>
         ) : (
           <>
-            <Button>
-              <Link href={`/${locale}/auth/signin`}>
-                <FormattedMessage id="links.login" />
-              </Link>
+            <Button
+              className={styles.button}
+              onClick={() => onHandleClick(`/${locale}/auth/signin`)}
+            >
+              <FormattedMessage id="links.login" />
             </Button>
-            <Button isPrimary={false}>
-              <Link href={`/${locale}/auth/signup`}>
-                <FormattedMessage id="links.register" />
-              </Link>
+            <Button isPrimary={false} onClick={() => onHandleClick(`/${locale}/auth/signup`)}>
+              <FormattedMessage id="links.register" />
             </Button>
           </>
         )}
