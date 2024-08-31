c989e42b834e2f9b572ce0f903466bedef9ccef0
anturchin <tur4intwo@gmail.com>
2024-08-31 22:03:02 +0300

    refactor: update styles

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import Image from 'next/image';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { IntlProvider } from 'react-intl';
 import { FormattedMessage } from 'react-intl';
@@ -9,6 +10,8 @@ import { usePathname, useRouter } from 'next/navigation';
 import { auth, logout } from '@/services/firebase';
 import { Button, LocaleSelector } from '@/components';
 import { IHeaderProps } from './header.props';
+
+import logo from './rest.svg';
 
 import styles from './header.module.css';
 
@@ -25,50 +28,55 @@ export const Header = ({ locale, messages }: IHeaderProps) => {
   return (
     <IntlProvider locale={locale} messages={messages}>
       <header className={styles.header}>
-        {user ? (
-          <>
-            <Link className={pathname === `/${locale}` ? styles.active : ''} href={`/${locale}`}>
-              <FormattedMessage id="links.home" />
-            </Link>
-            <Link
-              className={pathname === `/${locale}/rest/GET/json` ? styles.active : ''}
-              href={`/${locale}/rest/GET/json`}
-            >
-              <FormattedMessage id="links.rest" />
-            </Link>
-            <Link
-              className={pathname === `/${locale}/graphql/query` ? styles.active : ''}
-              href={`/${locale}/graphql/query`}
-            >
-              <FormattedMessage id="links.graphql" />
-            </Link>
-            <Link
-              className={pathname === `/${locale}/history` ? styles.active : ''}
-              href={`/${locale}/history`}
-            >
-              <FormattedMessage id="links.history" />
-            </Link>
-            <Button
-              onClick={() => {
-                void logout();
-              }}
-            >
-              <FormattedMessage id="links.logout" />
-            </Button>
-          </>
-        ) : (
-          <>
-            <Button
-              className={styles.button}
-              onClick={() => onHandleClick(`/${locale}/auth/signin`)}
-            >
-              <FormattedMessage id="links.login" />
-            </Button>
-            <Button isPrimary={false} onClick={() => onHandleClick(`/${locale}/auth/signup`)}>
-              <FormattedMessage id="links.register" />
-            </Button>
-          </>
-        )}
+        <Link className={styles.logo} href={`/${locale}`}>
+          <Image src={logo as string} alt="logo" width={80}></Image>
+        </Link>
+        <div className={styles.menu}>
+          {user ? (
+            <>
+              <Link className={pathname === `/${locale}` ? styles.active : ''} href={`/${locale}`}>
+                <FormattedMessage id="links.home" />
+              </Link>
+              <Link
+                className={pathname === `/${locale}/rest/GET/json` ? styles.active : ''}
+                href={`/${locale}/rest/GET/json`}
+              >
+                <FormattedMessage id="links.rest" />
+              </Link>
+              <Link
+                className={pathname === `/${locale}/graphql/query` ? styles.active : ''}
+                href={`/${locale}/graphql/query`}
+              >
+                <FormattedMessage id="links.graphql" />
+              </Link>
+              <Link
+                className={pathname === `/${locale}/history` ? styles.active : ''}
+                href={`/${locale}/history`}
+              >
+                <FormattedMessage id="links.history" />
+              </Link>
+              <Button
+                onClick={() => {
+                  void logout();
+                }}
+              >
+                <FormattedMessage id="links.logout" />
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                className={styles.button}
+                onClick={() => onHandleClick(`/${locale}/auth/signin`)}
+              >
+                <FormattedMessage id="links.login" />
+              </Button>
+              <Button isPrimary={false} onClick={() => onHandleClick(`/${locale}/auth/signup`)}>
+                <FormattedMessage id="links.register" />
+              </Button>
+            </>
+          )}
+        </div>
         <LocaleSelector locale={locale} />
       </header>
     </IntlProvider>
