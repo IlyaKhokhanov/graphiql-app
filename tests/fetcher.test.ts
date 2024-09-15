60badd07349711100da623c1c322ef7fea916307
CerBeer <ocelopp@gmail.com>
2024-09-15 12:07:23 +0300

    feat: add tests for graphql

--- tests/fetcher.test.ts
+++ tests/fetcher.test.ts
@@ -4,26 +4,45 @@ import fetchMock from 'fetch-mock';
 import { NextRequest } from 'next/server';
 import { fetcher } from '@/utils/fetcher';
 
+const url = 'http://localhost:3000';
+const options = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+} as unknown as NextRequest;
+
 afterEach(() => {
   fetchMock.restore();
 });
 
-fetchMock.mock('*', {
-  method: 'GET',
-  headers: [{ 'Content-Type': 'application/json' }],
-  body: '{"prop1": "val1", "prop2": "val2"}',
-});
-
 describe('Rest fetcher', () => {
-  it('Fetcher fetching', async () => {
-    const url = 'http://localhost:3000/api/graphQlProxy?url=https%3A%2F%2Frickandmortyapi.com%2Fgraphql';
-    const options = {
+  it('Fetcher fetching 200', async () => {
+    fetchMock.mock('*', {
       method: 'GET',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-    } as unknown as NextRequest;
-    const result = await fetcher({url, options});
+      headers: [{ 'Content-Type': 'application/json' }],
+      body: '{"prop1": "val1", "prop2": "val2"}',
+    });
+
+    const result = await fetcher({ url, options });
     expect(result.status).toBe(200);
+  });
+  it('Fetcher fetching 404', async () => {
+    fetchMock.mock('*', 404);
+
+    const result = await fetcher({ url, options });
+    expect(result.status).toBe(404);
+  });
+  it('Fetcher fetching body not json', async () => {
+    fetchMock.mock('*', 'prop1');
+
+    const result = await fetcher({ url, options });
+    expect(result.isError).toBe(true);
+  });
+  it('Fetcher fetching no response', async () => {
+    fetchMock.mock('*', 500);
+
+    const result = await fetcher({ url, options });
+    expect(result.message).toBe('Internal Server Error');
   });
 });
